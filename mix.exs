f47cb6a445358bb26ff54dad119060307dffb414
Sarat Chandra <saratchandra@outlook.com>
2016-07-27 01:17:20 +0530

    Remove Elixir v1.2 and make v1.3 the min required version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NatsEx.Mixfile do
   def project do
     [app: :nats_ex,
      version: "0.1.0",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
