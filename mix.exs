22c067d97a4552091513eb570525cfe54e451223
Sarat Chandra <saratchandra@outlook.com>
2017-07-03 23:23:06 +0530

    Update dependencies and make 1.4 as the minimum elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NatsEx.Mixfile do
   def project do
     [app: :nats_ex,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
@@ -17,7 +17,7 @@ defmodule NatsEx.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :poison, :gproc],
+    [applications: [:logger, :poison],
      mod: {NatsEx, []}]
   end
 
@@ -31,10 +31,9 @@ defmodule NatsEx.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:poison, "~> 2.2"},
-     {:gproc, "~> 0.5.0"},
-     {:ex_doc, "~> 0.12", only: :dev},
-     {:credo, "~> 0.4", only: [:dev, :test]}]
+    [{:poison, "~> 3.1"},
+     {:ex_doc, "~> 0.16", only: :dev},
+     {:credo, "~> 0.8", only: [:dev, :test]}]
   end
 
   defp description do
