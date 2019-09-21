846fafbf687a13fd5d5bcf31637246e3301d1e42
Sarat Chandra <me@saratchandra.in>
2019-09-21 10:35:31 +0530

    Update Elixir,OTP and Nats server version in Travis

--- install_gnatsd.sh
+++ install_gnatsd.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export DEFAULT_NATS_SERVER_VERSION=v1.2.0
+export DEFAULT_NATS_SERVER_VERSION=v2.1.0
 export NATS_SERVER_VERSION="${NATS_SERVER_VERSION:=$DEFAULT_NATS_SERVER_VERSION}"
 
 # check to see if gnatsd folder is empty
@@ -10,11 +10,11 @@ if [ ! "$(ls -A $HOME/nats-server)" ]; then
     (
 	      mkdir -p $HOME/nats-server
 	      cd $HOME/nats-server
-	      wget https://github.com/nats-io/gnatsd/releases/download/$NATS_SERVER_VERSION/gnatsd-$NATS_SERVER_VERSION-linux-amd64.zip -O nats-server.zip
+	      wget https://github.com/nats-io/nats-server/releases/download/$NATS_SERVER_VERSION/nats-server-$NATS_SERVER_VERSION-linux-amd64.zip -O nats-server.zip
 	      unzip nats-server.zip
-	      cp gnatsd-$NATS_SERVER_VERSION-linux-amd64/gnatsd $HOME/nats-server/gnatsd
+	      cp nats-server-$NATS_SERVER_VERSION-linux-amd64/nats-server $HOME/nats-server/nats-server
         # start gnatsd server
-        nohup $HOME/nats-server/gnatsd &
+        nohup $HOME/nats-server/nats-server &
     )
 else
     echo 'Using cached directory.';
